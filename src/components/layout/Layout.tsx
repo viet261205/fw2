256a48fbcd12eed65033dd2e8ae72be407dbd3a6
N_HA <hatpph50768@gmail.com>
2025-03-24 10:25:15 +0700

    footer, header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+// import React from 'react';
+// import Header from './Header';
+// import Footer from './Footer';
 
-interface LayoutProps {
-    children: React.ReactNode;
-}
+// interface LayoutProps {
+//     children: React.ReactNode;
+// }
 
-export default function Layout({ children }: LayoutProps) {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <main className="flex-grow">
-                {children}
-            </main>
-            <Footer />
-        </div>
-    );
-}
+// export default function Layout({ children }: LayoutProps) {
+//     return (
+//         <div className="min-h-screen flex flex-col">
+//             <Header />
+//             <main className="flex-grow">
+//                 {children}
+//             </main>
+//             <Footer />
+//         </div>
+//     );
+// }
